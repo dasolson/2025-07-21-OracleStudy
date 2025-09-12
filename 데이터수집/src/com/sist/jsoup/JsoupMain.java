d3df83bd5692e78d498c91b500f00f62264deab0
sist <sist@sist24>
2025-09-12 17:02:01 +0900

    데이터 수집

---
+++ 데이터수집/src/com/sist/jsoup/JsoupMain.java
@@ -0,0 +1,58 @@
+package com.sist.jsoup;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class JsoupMain {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+       String html="<html><body>"
+    		       +"<div class=k>"
+    		       +"<div class=aaa>java</div>"
+    		       +"<div id=bbb>Oracle</div>"
+    		       +"</div>"
+    		       +"<div class=m>"
+    		       +"<div class=aaa>JSP</div>"
+    		       +"<div id=bbb>HTML</div>"
+    		       +"</div>"
+    		       +"</body><html>";
+       Document doc=Jsoup.parse(html);
+       System.out.println(doc.toString());
+       /*
+        *  <div class=aaa>java</div>
+        *   --- --------- 
+        *   태그  태그 구분자 => class(중복 가능) / id(중복 불가)
+        *    
+        *     태그명.aaa , 태그명#bbb
+        *     --------   ---------
+        *      클래스         id
+        *     => 자바에서는 태그 : Element => 같은 태크 여러개를 모아서 처리
+        *                                 -----------------------
+        *                                   Elements                                  
+        */
+       Elements div=doc.select("div");
+       System.out.println(div);
+       /*
+        *     HTML은 데이터 저장
+        *      <img src="이미지주소"> => attr("src") => 이미지주소
+        *      <div>Hello</div> => text() => Hello
+        *      => 태그포함 가져올때 => html() 
+        *      
+        *      자바스크립트 값을 가져올때 => data()
+        */
+       for(int i = 0; i < div.size(); i++) { // div안에 있는 데이터 추출
+    	   System.out.println(div.get(i).text());
+       }
+       
+       Element aaa=doc.selectFirst("div.m div.aaa");
+       Element bbb=doc.selectFirst("div.m div#bbb");
+       System.out.println(aaa);
+       System.out.println(bbb);
+       System.out.println(aaa.text());
+       System.out.println(bbb.text());
+	}
+
+}
