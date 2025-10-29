47955f8c84c7a04906c1892092d86d36402f915a
sist <sist@sist24>
2025-10-29 17:49:05 +0900

    2025-10-29

---
+++ JSPMiddleProject_1/src/main/java/com/sist/dao/ReplyBoardDAO.java
@@ -0,0 +1,128 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.sist.vo.*;
+import com.sist.commons.*;
+public class ReplyBoardDAO {
+   private static SqlSessionFactory ssf;
+   static
+   {
+	   ssf=CreateSqlSessionFactory.getSsf();
+   }
+   /*
+    *   <select id="boardListData" resultType="ReplyBoardVO"
+   parameterType="int"
+  >
+    <!-- 
+           group_tab : 간격조절 
+             AAAAA 0
+               => BBBBB 1
+                 => CCCCC 2
+           group_id : 답변을 모아둔것 
+             AAAAA 1
+               => BBBBB 1
+                => CCCCCC 1
+               => DDDD 1
+             EEEE 2
+              => KKKK 2
+           group_step
+              AAAAA 0
+               => BBBBB 1
+                => CCCCCC 2
+               => DDDD 3
+               
+           AAAAAA           1  0
+             
+             => BBBBBB      1  1
+             
+               => CCCCCCC   1  2
+             => DDDDDD      1  3
+              
+               
+     -->
+    SELECT no,subject,name,
+           TO_CHAR(regdate,'YYYY-MM-DD') as dbday,
+           hit,group_tab
+    FROM replyboard
+    ORDER BY group_id DESC,group_step ASC
+    OFFSET #{start} ROWS NEXT 10 ROWS ONLY
+    
+  </select>
+  <select id="boardTotalPage" resultType="int">
+   SELECT CEIL(COUNT(*)/10.0)
+   FROM replyboard
+  </select>
+    */
+   public static List<ReplyBoardVO> boardListData(int start)
+   {
+	   SqlSession session=ssf.openSession(); // 오라클 연결 
+	   List<ReplyBoardVO> list=session.selectList("boardListData",start);
+	   session.close(); // 오라클 종료 
+	   return list;
+   }
+   
+   public static int boardTotalPage()
+   {
+	   SqlSession session=ssf.openSession(); // 오라클 연결 
+	   int total=session.selectOne("boardTotalPage");
+	   session.close(); // 오라클 종료 
+	   return total;
+   }
+   
+   public static void boardInsert(ReplyBoardVO vo)
+   {
+	   SqlSession session=ssf.openSession(true);
+	   session.insert("boardInsert",vo);
+	   session.close();
+   }
+   /*
+    *   <update id="boardHitIncrement" parameterType="int">
+      UPDATE replyboard SET
+      hit=hit+1 
+      WHERE no=#{no}
+    </update>
+    <select id="boardDetailData" resultType="ReplyBoardVO"
+     parameterType="int"
+    >
+      SELECT no,name,subject,content,hit,
+             TO_CHAR(regdate,'YYYY-MM-DD HH24:MI:SS') as dbday
+      FROM replyboard
+      WHERE no=#{no}
+    </select>
+    */
+   public static ReplyBoardVO boardDetailData(int no)
+   {
+	   SqlSession session=ssf.openSession();
+	   session.update("boardHitIncrement",no);
+	   session.commit();
+	   ReplyBoardVO vo=
+			   session.selectOne("boardDetailData",no);
+	   session.close();
+	   return vo;
+   }
+   /*
+    *    <select id="boardUpdateData" resultType="ReplyBoardVO" parameterType="int">
+	       SELECT no, subject, content
+	       FROM replyboard
+	       WHERE no=#{no}
+	    </select>
+	    <select id="boardGetPassword" resultType="String" parameterType="int">
+	       SELECT pwd FROM replyBoard
+	       WHERE no=#{no}
+	    </select>
+	    <update id="boardUpdate" parameterType="ReplyBoardVO">
+	       UPDATE replyboard SET
+	       name=#{name}, subject=#{subject}, content=#{content}
+	       WHERE no=#{no}
+	    </update>
+    */
+   public static ReplyBoardVO boardUpdateData(int no) {
+	   SqlSession session = ssf.openSession();
+	   ReplyBoardVO vo = session.selectOne("boardUpdateData", no);
+	   session.close();
+	   return vo;
+   }
+}
---
+++ JSPMiddleProject_1/src/main/java/com/sist/model/ReplyBoardModel.java
@@ -0,0 +1,101 @@
+package com.sist.model;
+
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+import com.sist.vo.*;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.sist.dao.*;
+@Controller
+public class ReplyBoardModel {
+  @RequestMapping("board/list.do")
+  public String board_list(HttpServletRequest request,
+		  HttpServletResponse response) {
+	  String page = request.getParameter("page");
+	  // HttpServletRequest는 사용하지 않는다 
+	  // 스프링 board_list(int page)
+	  // 스프링 board_insert(BoardVO vo)
+	  if(page == null)
+		  page = "1";
+	  int curpage = Integer.parseInt(page);
+	  List<ReplyBoardVO> list = ReplyBoardDAO.boardListData((curpage-1)*10);
+	  int count = ReplyBoardDAO.boardTotalPage();
+	  int totalpage = (int)(Math.ceil(count/10.0));
+	  count = count-((curpage*10)-10);
+	  request.setAttribute("today", new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+	  request.setAttribute("count", count);
+	  request.setAttribute("list", list);
+	  request.setAttribute("curpage", curpage);
+	  request.setAttribute("totalpage", totalpage);
+	  request.setAttribute("main_jsp", "../replyboard/list.jsp");
+	  return "../main/main.jsp";
+  }
+  @RequestMapping("board/insert.do")
+  public String board_insert(HttpServletRequest request,
+		  HttpServletResponse response) {
+	  request.setAttribute("main_jsp", "../replyboard/insert.jsp");
+	  return "../main/main.jsp";
+  }
+  @RequestMapping("board/insert_ok.do")
+  public String board_insert_ok(HttpServletRequest request,
+		  HttpServletResponse response) {
+	  // 처리 
+	  //1. 사용자가 보낸 데이터 받기  name , subject, content,pwd
+	  String name = request.getParameter("name");
+	  String subject = request.getParameter("subject");
+	  String content = request.getParameter("content");
+	  String pwd = request.getParameter("pwd");
+	  
+	  ReplyBoardVO vo = new ReplyBoardVO();
+	  vo.setName(name);
+	  vo.setContent(content);
+	  vo.setSubject(subject);
+	  vo.setPwd(pwd);
+	  
+	  // vo=>DAO : 오라클 저장 
+	  ReplyBoardDAO.boardInsert(vo);
+	  // 화면 이동 
+	  // sendRedirect() => 화면 이동 (request를 초기화)
+	  // 재호출 => list.do 다시 호출한다
+	  return "redirect:../board/list.do";
+  }
+  @RequestMapping("board/detail.do")
+  public String board_detail(HttpServletRequest request,
+		  HttpServletResponse response) {
+	  // 사용자 전송 데이터 받기 
+	  // ?no=10
+	  String no = request.getParameter("no");
+	  // 요청 처리 => DAO
+	  ReplyBoardVO vo = ReplyBoardDAO.boardDetailData(Integer.parseInt(no));
+	  // 처리된 결과값 => 해당 JSP전송 
+	  request.setAttribute("vo", vo);
+	  // 화면 출력 
+	  request.setAttribute("main_jsp", "../replyboard/detail.jsp");
+	  return "../main/main.jsp";
+  }
+  @RequestMapping("board/update.do")
+  public String board_update(HttpServletRequest request,
+		  HttpServletResponse response) {
+	  String no = request.getParameter("no");
+	  ReplyBoardVO vo = ReplyBoardDAO.boardUpdateData(Integer.parseInt(no));
+	  // update.jsp로 전송
+	  request.setAttribute("vo", vo);
+	  request.setAttribute("main_jsp", "../replyboard/update.jsp");
+	  return "../main/main.jsp";
+  }
+  @RequestMapping("board/reply.do")
+  public String board_reply(HttpServletRequest request,
+		  HttpServletResponse response) {
+	  
+	  request.setAttribute("main_jsp", "../replyboard/reply.jsp");
+	  return "../main/main.jsp";
+  }
+  
+  
+  
+}
